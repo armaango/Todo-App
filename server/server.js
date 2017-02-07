2057eb46e1d47bd356826ce0ac229de1980a29d8
armaango <armaan.goyal@gmail.com>
2017-02-07 14:50:20 -0800

    first HTTP endpoint setup

--- server/server.js
+++ server/server.js
@@ -1,4 +1,27 @@
-var{mongoose} = require('./db/mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var{Todo} = require('./models/todo');
-var{User} = require('./models/user');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started server on port 3000');
+});
