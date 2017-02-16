43dca8ee345604acb39e0913795e38caabaadf6d
armaango <armaan.goyal@gmail.com>
2017-02-16 01:24:53 -0800

    Add authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,27 @@ return user.save().then(()=>{
 });
 };
 
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+
+    try {
+        decoded = jwt.verify(token,'abc1234');
+    } catch (e) {
+        // return new Promise((resolve,reject)=>{
+        //     reject();
+        // });
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access':'auth'
+    });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -115,8 +116,9 @@ app.post('/users', (req, res) => {
     })
 });
 
-app.get('/users/me',(req,res) => {
-    var token = req.header();
+
+app.get('/users/me', authenticate, (req,res) => {
+    res.send(req.user);
 });
 
 
